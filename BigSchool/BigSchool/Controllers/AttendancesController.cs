40b4bad508c9213a8ed4e7ff7828fa72dd1adae2
xuanquang123 <76514491+xuanquang123@users.noreply.github.com>
2021-07-12 23:28:16 +0700

    Bai moi

---
+++ BigSchool/BigSchool/Controllers/AttendancesController.cs
@@ -0,0 +1,29 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BigSchool.Controllers
+{
+    public class AttendancesController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Attend(Course attendanceDto)
+        {
+            var userID = User.Identity.GetUserId();
+            BigSchoolContext db = new BigSchoolContext();
+            if (db.Attendances.Any(prop=>prop.Attendee == userID && prop.CourseId == attendanceDto.Id))
+            {
+                return BadRequest("The attendance already exists!");
+            }
+            var attendance = new Attendance() { CourseId = attendanceDto.Id, Attendee = User.Identity.GetUserId() };
+            db.Attendances.Add(attendance);
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ BigSchool/BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,106 @@
+﻿using BigSchool.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        // GET: Courses
+        public ActionResult Create()
+        {
+            BigSchoolContext db = new BigSchoolContext();
+            Course objCourse = new Course();
+            objCourse.ListCategory = db.Categories.ToList();
+            return View(objCourse);
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course objCourse)
+        {
+            BigSchoolContext db = new BigSchoolContext();
+            ModelState.Remove("LecturerId");
+            if (!ModelState.IsValid)
+            {
+                objCourse.ListCategory = db.Categories.ToList();
+                return View("Create", objCourse);
+            }
+
+            ApplicationUser user = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>().FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            objCourse.LecturerId = user.Id;
+
+            db.Courses.Add(objCourse);
+            db.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Attending()
+        {
+            BigSchoolContext db = new BigSchoolContext();
+            ApplicationUser currentUser = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>()
+                .FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            var listAttendances = db.Attendances.Where(prop => prop.Attendee == currentUser.Id).ToList();
+            var courses = new List<Course>();
+            foreach (Attendance temp in listAttendances)
+            {
+                Course objCourse = temp.Course;
+                objCourse.LectureName = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>()
+                    .FindById(objCourse.LecturerId).Name;
+                courses.Add(objCourse);
+            }
+            return View(courses);
+        }
+
+        public ActionResult Mine()
+        {
+            ApplicationUser currentUser = System.Web.HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>()
+                .FindById(System.Web.HttpContext.Current.User.Identity.GetUserId());
+            BigSchoolContext db = new BigSchoolContext();
+            var courses = db.Courses.Where(c => c.LecturerId == currentUser.Id && c.DateTime > DateTime.Now).ToList();
+            foreach (Course i in courses)
+            {
+                i.LectureName = currentUser.Name;
+            }
+            return View(courses);
+        }
+        public ActionResult Edit(int id)
+        {
+            BigSchoolContext db = new BigSchoolContext();
+            return View(db.Courses.Where(c=>c.Id==id).FirstOrDefault());
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Course c)
+        {
+            BigSchoolContext db = new BigSchoolContext();
+            db.Entry(c).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Mine", "Courses");
+        }
+        public ActionResult Delete(int id)
+        {
+            BigSchoolContext db = new BigSchoolContext();
+            return View(db.Courses.Where(c => c.Id == id).FirstOrDefault());
+        }
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Course course)
+        {
+            BigSchoolContext db = new BigSchoolContext();
+            course = db.Courses.FirstOrDefault(c => c.Id == id);
+            db.Courses.Remove(course);
+            db.SaveChanges();
+            return RedirectToAction("Mine", "Courses");
+        }
+    }
+}
